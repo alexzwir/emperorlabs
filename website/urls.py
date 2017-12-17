ba86b6f76d007109bd6d5599a910ec40cd6aaf73
Alex Zwir <alexzwir@gmail.com>
2017-12-17 18:11:36 -0200

    Adding the first view of the project for testing.

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from website.core import views
+
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'website.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$/', core.views),
     url(r'^admin/', include(admin.site.urls)),
 ]
